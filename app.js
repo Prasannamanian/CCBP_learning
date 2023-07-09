cdfa7ae51c6b717f05902e37c2b03c2497891ec2
prasannamanian <prasannamanian14@gmail.com>
2023-07-09 23:25:09 +0000

    coding practice 8

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const initializeDBAndServer = async () => {
       filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3001, () => {
+    app.listen(3002, () => {
       console.log("Server Running Successfully!!!");
     });
   } catch (e) {
@@ -64,6 +64,77 @@ app.get("/todos/", async (request, response) => {
 
   const data = await db.all(getQuery);
   response.send(data);
+});
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getQuery = `
+    SELECT
+        *
+    FROM
+        todo
+    WHERE
+        id = ${todoId};`;
+  const data = await db.get(getQuery);
+  response.send(data);
+});
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status } = request.body;
+  const putQuery = `
+    INSERT INTO todo (id, todo, priority, status)
+    VALUES (${id},'${todo}','${priority}','${status}');`;
+  await db.run(putQuery);
+  response.send("Todo Successfully Added");
+});
+
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const { status, priority, todo } = request.body;
+  let putQuery = "";
+  let updatedTodo = "";
+  const previousTodo = `SELECT * FROM todo WHERE id = ${todoId};`;
+  const previousTodoData = await db.get(previousTodo);
+  const { prev_status, prev_priority, prev_todo } = previousTodoData;
+
+  if (status !== undefined) {
+    putQuery = `
+    UPDATE 
+        todo 
+    SET 
+        todo = '${prev_todo}',
+        priority = '${prev_priority}',
+        status = '${status}'
+    WHERE 
+        id = ${todoId};`;
+    updatedTodo = "Status";
+  } else if (priority !== undefined) {
+    putQuery = `
+    UPDATE 
+        todo 
+    SET 
+        todo = '${prev_todo}',
+        priority = '${priority}',
+        status = '${prev_status}'
+    WHERE
+        id = ${todoId};`;
+    updatedTodo = "Priority";
+  } else if (todo !== undefined) {
+    putQuery = `
+    UPDATE 
+        todo 
+    SET 
+        todo = '${todo}',
+        priority = '${prev_priority}',
+        status = '${prev_status}' 
+    WHERE
+        id = ${todoId};`;
+    updatedTodo = "Todo";
+  }
+  console.log(putQuery);
+  console.log(updated);
+  await db.run(putQuery);
+  response.send(`${updatedTodo} Updated`);
 });
 
 module.exports = app;
